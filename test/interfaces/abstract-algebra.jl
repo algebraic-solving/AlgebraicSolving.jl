3c4a39748eb7b5d949e623ac90575cc812e4c82b
ederc <ederc@mathematik.uni-kl.de>
2022-07-02 15:54:20 +0200

    fixes interface tests of internal funcitonality

--- test/interfaces/abstract-algebra.jl
+++ test/interfaces/abstract-algebra.jl
@@ -2,13 +2,13 @@
     R, (x,y,z) = PolynomialRing(QQ,["x","y","z"], ordering=:degrevlex)
     F = [x^2+1-3, x*y-z, x*z^2-3*y^2]
     cmp = (Int32[2, 2, 2], BigInt[1, 1, -2, 1, 1, 1, -1, 1, 1, 1, -3, 1], Int32[2, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 2, 0, 2, 0])
-    @test AlgebraicSolving.convert_to_msolve(F) == cmp
+    @test AlgebraicSolving._convert_to_msolve(F) == cmp
     R, (x,y,z) = PolynomialRing(GF(2147483659),["x","y","z"], ordering=:degrevlex)
     F = [x^2+1-3, x*y-z, x*z^2-3*y^2]
     # prime is bigger than 2^31, should throw an error
-    @test_throws ErrorException AlgebraicSolving.convert_to_msolve(F)
+    @test_throws ErrorException AlgebraicSolving._convert_to_msolve(F)
     R, (x,y,z) = PolynomialRing(GF(101),["x","y","z"], ordering=:degrevlex)
     F = [x^2+1-3, x*y-z, x*z^2-3*y^2]
-    res = AlgebraicSolving.convert_to_msolve(F)
-    @test AlgebraicSolving.convert_finite_field_gb_to_abstract_algebra(Int32(3), res..., R) == F
+    res = AlgebraicSolving._convert_to_msolve(F)
+    @test AlgebraicSolving._convert_finite_field_gb_to_abstract_algebra(Int32(3), res..., R) == F
 end
