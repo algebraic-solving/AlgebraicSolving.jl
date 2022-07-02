ce5c0aa89e724fd8591a279e12fdf59a51361a53
ederc <ederc@mathematik.uni-kl.de>
2022-07-02 15:34:26 +0200

    adjusts documentation for msolve functionality

--- src/algorithms/solvers.jl
+++ src/algorithms/solvers.jl
@@ -3,13 +3,13 @@ import msolve_jll: libmsolve
 export real_solutions, rational_parametrization
 
 @doc Markdown.doc"""
-    get_rational_parametrization(nr::Int32, lens::Vector{Int32}, cfs::Ptr{BigInt})
+    _get_rational_parametrization(nr::Int32, lens::Vector{Int32}, cfs::Ptr{BigInt})
 
 Construct the rational parametrization of the solution set computed via msolve.
 
 **Note**: This is an internal function and should only be used inside `msolve()`.
 """
-function get_rational_parametrization(
+function _get_rational_parametrization(
         nr::Int32,
         lens::Vector{Int32},
         cfs::Ptr{BigInt}
@@ -38,34 +38,12 @@ function get_rational_parametrization(
 end
 
 @doc Markdown.doc"""
-    msolve(I::MPolyIdeal, <keyword arguments>)
+    _core_msolve(F::Vector{T} where T <: MPolyElem, <keyword arguments>)
 
-Given an ideal `I` with a finite solution set over the complex numbers, return a pair `p,r` where `p` is the rational parametrization of the solution set and `r` represents the real roots of `Ì`  with a given precision (default 32 bits).
-See [BES21](@cite) for more information.
+Compute a rational parametrization and the real solutions of an ideal given by
+the generators `F` via msolve.
 
-**Note**: At the moment only QQ is supported as ground field. If the dimension of `I`
-is greater then zero an empty array is returned.
-
-# Arguments
-- `Ì::MPolyIdeal`: input ideal.
-- `initial_hts::Int=17`: initial hash table size `log_2`.
-- `nr_thrds::Int=1`: number of threads for parallel linear algebra.
-- `max_nr_pairs::Int=0`: maximal number of pairs per matrix, only bounded by minimal degree if `0`.
-- `la_option::Int=2`: linear algebra option: exact sparse-dense (`1`), exact sparse (`2`, default), probabilistic sparse-dense (`42`), probabilistic sparse(`44`).
-- `info_level::Int=0`: info level printout: off (`0`, default), summary (`1`), detailed (`2`).
-- `precision::Int=32`: bit precision for the computed solutions.
-
-# Examples
-```jldoctest
-julia> R,(x1,x2,x3) = PolynomialRing(QQ, ["x1","x2","x3"])
-(Multivariate Polynomial Ring in x1, x2, x3 over Rational Field, fmpq_mpoly[x1, x2, x3])
-
-julia> I = ideal(R, [x1+2*x2+2*x3-1, x1^2+2*x2^2+2*x3^2-x1, 2*x1*x2+2*x2*x3-x2])
-ideal(x1 + 2*x2 + 2*x3 - 1, x1^2 - x1 + 2*x2^2 + 2*x3^2, 2*x1*x2 + 2*x2*x3 - x2)
-
-julia> msolve(I)
-((84*x^4 - 40*x^3 + x^2 + x, 336*x^3 - 120*x^2 + 2*x + 1, PolyElem[-184*x^3 + 80*x^2 - 4*x - 1, -36*x^3 + 18*x^2 - 2*x], fmpz[-1, -1]), Vector{fmpq}[[744483363399261433351//1180591620717411303424, 372241681699630716673//1180591620717411303424, -154187553040555781639//1180591620717411303424], [1, 0, 0], [71793683196126133110381699745//316912650057057350374175801344, 71793683196126133110381699745//633825300114114700748351602688, 173325283664805084153412401855//633825300114114700748351602688], [196765270119568550571//590295810358705651712, 1//590295810358705651712, 196765270119568550571//590295810358705651712]])
-```
+**Note**: This is an internal function.
 """
 function _core_msolve(
         F::Vector{T} where T <: MPolyElem;    # input generators
@@ -144,7 +122,7 @@ function _core_msolve(
     # if 0 == field_char
     jl_cf       = reinterpret(Ptr{BigInt}, res_cf[])
 
-    rat_param = get_rational_parametrization(jl_ld, jl_len, jl_cf)
+    rat_param = _get_rational_parametrization(jl_ld, jl_len, jl_cf)
 
     if jl_nb_sols == 0
         return rat_param, Vector{fmpq}[]
@@ -188,6 +166,40 @@ function _core_msolve(
     return rat_param, solutions
 end
 
+@doc Markdown.doc"""
+    rational_parametrization(F::Vector{T} where T <: MPolyElem, <keyword arguments>)
+
+Given an ideal by generators `F` with a finite solution set over the complex numbers, return
+the rational parametrization of the ideal with a given precision (default 32 bits).
+
+**Note**: At the moment only QQ is supported as ground field. If the dimension of the ideal
+is greater then zero an empty array is returned.
+
+# Arguments
+- `F::Vector{T} where T <: MPolyElem`: input generators.
+- `initial_hts::Int=17`: initial hash table size `log_2`.
+- `nr_thrds::Int=1`: number of threads for parallel linear algebra.
+- `max_nr_pairs::Int=0`: maximal number of pairs per matrix, only bounded by minimal degree if `0`.
+- `la_option::Int=2`: linear algebra option: exact sparse-dense (`1`), exact sparse (`2`, default), probabilistic sparse-dense (`42`), probabilistic sparse(`44`).
+- `info_level::Int=0`: info level printout: off (`0`, default), summary (`1`), detailed (`2`).
+- `precision::Int=32`: bit precision for the computed solutions.
+
+# Examples
+```jldoctest
+julia> R,(x1,x2,x3) = PolynomialRing(QQ, ["x1","x2","x3"])
+(Multivariate Polynomial Ring in x1, x2, x3 over Rational Field, fmpq_mpoly[x1, x2, x3])
+
+julia> F = [x1+2*x2+2*x3-1, x1^2+2*x2^2+2*x3^2-x1, 2*x1*x2+2*x2*x3-x2]
+ideal(x1 + 2*x2 + 2*x3 - 1, x1^2 - x1 + 2*x2^2 + 2*x3^2, 2*x1*x2 + 2*x2*x3 - x2)
+
+julia> real_solutions(F)
+4-element Vector{Vector{Nemo.fmpq}}:
+ [744483363399261433351//1180591620717411303424, 372241681699630716673//1180591620717411303424, -154187553040555781639//1180591620717411303424]
+ [1, 0, 0]
+ [71793683196126133110381699745//316912650057057350374175801344, 71793683196126133110381699745//633825300114114700748351602688, 173325283664805084153412401855//633825300114114700748351602688]
+ [196765270119568550571//590295810358705651712, 1//590295810358705651712, 196765270119568550571//590295810358705651712]
+```
+"""
 function rational_parametrization(
         F::Vector{T} where T <: MPolyElem;    # input generators
         initial_hts::Int=17,                  # hash table size, default 2^17
@@ -208,6 +220,36 @@ function rational_parametrization(
     return parametrization
 end
 
+@doc Markdown.doc"""
+    real_solutions(F::Vector{T} where T <: MPolyElem, <keyword arguments>)
+
+Given an ideal by generators `F` with a finite solution set over the complex numbers, return
+the real roots of the ideal with a given precision (default 32 bits).
+
+**Note**: At the moment only QQ is supported as ground field. If the dimension of the ideal
+is greater then zero an empty array is returned.
+
+# Arguments
+- `F::Vector{T} where T <: MPolyElem`: input generators.
+- `initial_hts::Int=17`: initial hash table size `log_2`.
+- `nr_thrds::Int=1`: number of threads for parallel linear algebra.
+- `max_nr_pairs::Int=0`: maximal number of pairs per matrix, only bounded by minimal degree if `0`.
+- `la_option::Int=2`: linear algebra option: exact sparse-dense (`1`), exact sparse (`2`, default), probabilistic sparse-dense (`42`), probabilistic sparse(`44`).
+- `info_level::Int=0`: info level printout: off (`0`, default), summary (`1`), detailed (`2`).
+- `precision::Int=32`: bit precision for the computed solutions.
+
+# Examples
+```jldoctest
+julia> R,(x1,x2,x3) = PolynomialRing(QQ, ["x1","x2","x3"])
+(Multivariate Polynomial Ring in x1, x2, x3 over Rational Field, fmpq_mpoly[x1, x2, x3])
+
+julia> F = [x1+2*x2+2*x3-1, x1^2+2*x2^2+2*x3^2-x1, 2*x1*x2+2*x2*x3-x2]
+ideal(x1 + 2*x2 + 2*x3 - 1, x1^2 - x1 + 2*x2^2 + 2*x3^2, 2*x1*x2 + 2*x2*x3 - x2)
+
+julia> rational_parametrization(F)
+(84*x^4 - 40*x^3 + x^2 + x, 336*x^3 - 120*x^2 + 2*x + 1, AbstractAlgebra.PolyElem[-184*x^3 + 80*x^2 - 4*x - 1, -36*x^3 + 18*x^2 - 2*x], Nemo.fmpz[-1, -1])
+```
+"""
 function real_solutions(
         F::Vector{T} where T <: MPolyElem;    # input generators
         initial_hts::Int=17,                  # hash table size, default 2^17
