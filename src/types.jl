32de582ee884d9d71cd1689bdf9fb38559a10890
ederc <ederc@mathematik.uni-kl.de>
2022-07-13 21:22:04 +0200

    adds types file

---
+++ src/types.jl
@@ -0,0 +1,47 @@
+export Ideal
+
+mutable struct RationalParametrization
+    vars::Vector{Symbol}
+    lf_cfs::Vector{fmpq}
+    elim::fmpq_poly
+    denom::fmpq_poly
+    param::Vector{PolyElem}
+
+    function RationalParametrization(
+            vars::Vector{Symbol},
+            lf_cfs::Vector{fmpq},
+            elim::fmpq_poly,
+            denom::fmpq_poly,
+            param::Vector{PolyElem}
+        )
+        rp = new()
+        rp.vars   = vars
+        rp.lf_cfs = lf_cfs
+        rp.elim   = elim
+        rp.denom  = denom
+        rp.param  = param
+
+        return rp
+    end
+end
+
+mutable struct Ideal{T <: MPolyElem}
+    gens::Vector{T}
+    dim::Int
+    gb::Dict{Int, Vector{T}}
+    real_sols::Vector{Vector{fmpq}}
+    rat_param::RationalParametrization
+
+    function Ideal(F::Vector{T}) where {T <: MPolyElem}
+        I = new{T}()
+        I.gens = F
+        I.dim  = -1
+        I.gb   = Dict()
+
+        return I
+    end
+end 
+
+Base.parent(I::Ideal) = Nemo.parent(I.gens[1])
+
+Base.show(io::IO, I::Ideal) = print(io, I.gens)
