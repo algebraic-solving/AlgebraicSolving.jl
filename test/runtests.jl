6662da4b26c184c342604804bbf62864ca42e1fd
ederc <ederc@mathematik.uni-kl.de>
2023-11-05 14:44:57 +0100

    updates tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using Test
 @testset verbose = true "AlgebraicSolving Tests" begin
 include("interfaces/nemo.jl")
 include("algorithms/groebner-bases.jl")
+include("algorithms/normal-forms.jl")
 include("algorithms/solvers.jl")
 include("examples/katsura.jl")
 end
