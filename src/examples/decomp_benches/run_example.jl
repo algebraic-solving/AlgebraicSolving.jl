b07013677b93d6ca066138f7ea4cdb0baa6b72cb
Rafael Mohr <Rafael.Mohr@lip6.fr>
2024-02-15 12:03:08 +0100

    add info level

--- src/examples/decomp_benches/run_example.jl
+++ src/examples/decomp_benches/run_example.jl
@@ -2,7 +2,7 @@ Fhom = homogenize(F);
 S = parent(first(Fhom));
 x, y, z = AlgebraicSolving.gens(S)[1:3]
 tst = [x*y, x*z, y*z];
-sig_decomp(tst);
+sig_decomp(tst, info_level = 1);
 println("----------")
-tm = @elapsed bla = sig_decomp(Fhom);
+tm = @elapsed bla = sig_decomp(Fhom, info_level = 1);
 println("time $(tm), $(length(bla)) components")
