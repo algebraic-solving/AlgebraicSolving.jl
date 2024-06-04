163d80f682256e7a228fed89beaa138022ea8e08
ederc <ederc@mathematik.uni-kl.de>
2024-06-04 15:08:19 +0200

    adds issue #54 as interface test, adjusts other tests correspondingly

--- test/interfaces/nemo.jl
+++ test/interfaces/nemo.jl
@@ -1,8 +1,13 @@
 @testset "Interfaces -> Nemo" begin
     R, (x,y,z) = polynomial_ring(QQ,["x","y","z"], internal_ordering=:degrevlex)
     F = [x^2+1-3, x*y-z, x*z^2-3*y^2]
-    cmp = (Int32[2, 2, 2], BigInt[1, 1, -2, 1, 1, 1, -1, 1, 1, 1, -3, 1], Int32[2, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 2, 0, 2, 0])
+    cmp = (Int32[2, 2, 2], BigInt[1, 1, -2, 1, 1, 1, -1, 1, 1, 1, -3, 1], Int32[2, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 2, 0, 2, 0], 3)
     @test AlgebraicSolving._convert_to_msolve(F) == cmp
+    # issue #54
+    R, (x1, x2) = polynomial_ring(GF(17), ["x1", "x2"])
+    I = Ideal([x1, R(0)])
+    cmp = (Int32[1], BigInt[1], Int32[1, 0], 1) 
+    @test AlgebraicSolving._convert_to_msolve(I.gens) == cmp
     for _GF in [GF, AlgebraicSolving.Nemo.Native.GF]
       R, (x,y,z) = polynomial_ring(_GF(2147483659),["x","y","z"], internal_ordering=:degrevlex)
       F = [x^2+1-3, x*y-z, x*z^2-3*y^2]
@@ -11,6 +16,6 @@
       R, (x,y,z) = polynomial_ring(_GF(101),["x","y","z"], internal_ordering=:degrevlex)
       F = [x^2+1-3, x*y-z, x*z^2-3*y^2]
       res = AlgebraicSolving._convert_to_msolve(F)
-      @test AlgebraicSolving._convert_finite_field_array_to_abstract_algebra(Int32(3), res..., R) == F
+      @test AlgebraicSolving._convert_finite_field_array_to_abstract_algebra(Int32(3), res[1], res[2], res[3], R) == F
     end
 end
