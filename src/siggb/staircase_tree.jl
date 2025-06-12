749b91ddb3e9d5e164ece099787f600658148d64
Théo Ternier <theoternier@PCdeTho.localdomain>
2025-06-12 14:16:00 +0200

    New version of the file staircase_tree.jl

--- src/siggb/staircase_tree.jl
+++ src/siggb/staircase_tree.jl
@@ -1,11 +1,11 @@
-# Change monomial encoding: Vector{Int} to Monomial{N}
-
-const Exp = Int16
-
 # Define a Tree structure to represent the staircase tree
 struct Tree
     id::Int
     edges::Vector{Tuple{Exp, Tree}}
+end
+
+function Base.hash(tree::Tree, h::UInt)
+    return hash(tree.id, h)
 end
 
 function Base.:(==)(t1::Tree, t2::Tree)
@@ -41,35 +41,46 @@ function new_hashstate()
     return HashState(Dict{Vector{Edge}, Tree}(), 0)
 end
 
+function troncate!(children::Vector{Edge}, i::Int)
+    if length(children) > i
+        resize!(children,i)
+    end
+end
+
 # Function to create a trees with hash consing
-function make_Tree(children::Vector{Edge}, hashstate::HashState)
-    edges = []
-    last = nothing
-    for child in children
-        if isnothing(last) || child[2] != last
-            push!(edges, child)
-            last = child[2]
+function make_tree(children::Vector{Edge}, hashstate::HashState)
+    i = 0
+    last = nothing_tree
+    for j in 1:length(children)
+        current_edge = children[j][2]
+        if current_edge != last
+            last = current_edge
+            children[i+1] = children[j]
+            i += 1
         end
     end
 
-    if haskey(hashstate.hashtable, edges)
-        return hashstate.hashtable[edges]
+    troncate!(children, i)
+    key = get(hashstate.hashtable, children, nothing_tree)
+
+    if key != nothing_tree
+        return hashstate.hashtable[children]
     end
 
-    tree = Tree(hashstate.counter, edges)
+    tree = Tree(hashstate.counter, children)
+    hashstate.hashtable[children] = tree
     hashstate.counter += 1
-    hashstate.hashtable[edges] = tree
     return tree
 end
 
 # Function to insert a prefix in the tree
-function insert_aux(tree::Tree, m::Vector{Exp}, i::Int, hashstate::HashState)
-    if m == [] || i == 1
+function insert_aux(tree::Tree, m::Monomial, i::Int, hashstate::HashState)
+    if isempty(m.exps) || i == 1
         return tree
     end
 
     i -= 1
-    a = m[i]
+    a = m.exps[i]
     newtree = Edge[]
     inserted = false
 
@@ -82,29 +93,30 @@ function insert_aux(tree::Tree, m::Vector{Exp}, i::Int, hashstate::HashState)
         end
     end
     if !inserted
-        push!(newtree, (a,insert_aux(make_Tree(Edge[], hashstate),m,i,hashstate)))
+        push!(newtree, (a,insert_aux(make_tree(Edge[], hashstate),m,i,hashstate)))
     end
-    return make_Tree(newtree, hashstate)
+    return make_tree(newtree, hashstate)
 end
 
-function insert(tree::Tree, m::Vector{Exp}, hashstate::HashState)
-    return insert_aux(tree, m, length(m)+1, hashstate)
+function insert(tree::Tree, m::Monomial, hashstate::HashState)
+    return insert_aux(tree, m, length(m.exps)+1, hashstate)
 end
 
 
 # Function to insert a monomial into the tree with memoization
-function insertion_aux(tree::Tree, m::Vector{Exp}, i::Int, memo::Dict{Tuple{Tree,Int}, Tree}, hashstate::HashState)
+function insertion_aux(tree::Tree, m::Monomial, i::Int, memo::Dict{Tuple{Tree,Int}, Tree}, hashstate::HashState)
     if tree == nothing_tree
-        return insert_aux(make_Tree(Edge[], hashstate), m, i, hashstate)
+        return insert_aux(make_tree(Edge[], hashstate), m, i, hashstate)
     end
     if i == 1
+         return tree
+    end
+    if isempty(tree.edges)
         return tree
     end
-    if tree == make_Tree(Edge[], hashstate)
-        return tree
-    end
-    if haskey(memo, (tree, i))
-        return memo[(tree, i)]
+    key = get(memo, (tree, i), nothing_tree)
+    if key != nothing_tree
+        return key
     end
 
     i -= 1
@@ -112,33 +124,38 @@ function insertion_aux(tree::Tree, m::Vector{Exp}, i::Int, memo::Dict{Tuple{Tree
     is_subtree = false
     last_subtree = nothing_tree
     for edge in tree.edges
-        if edge[1] < m[i]
+        if edge[1] < m.exps[i]
             push!(newtree, edge)
             last_subtree = edge[2]
         else
-            if edge[1] == m[i]
+            if edge[1] == m.exps[i]
                 is_subtree = true
             end
+            if edge[1] > m.exps[i] && !is_subtree
+                push!(newtree, (m.exps[i], insertion_aux(last_subtree, m, i, memo, hashstate)))
+                is_subtree = true
+            end
+
             push!(newtree, (edge[1], insertion_aux(edge[2], m, i, memo, hashstate)))
         end
     end
-            
+
     if !is_subtree
-        push!(newtree, (m[i], insertion_aux(last_subtree, m, i, memo, hashstate)))
-        newtree = sort(newtree, by = x -> x[1])
+        push!(newtree, (m.exps[i], insertion_aux(last_subtree, m, i, memo, hashstate)))
     end
-    final_tree = make_Tree(newtree, hashstate)
+
+    final_tree = make_tree(newtree, hashstate)
     memo[(tree, i)] = final_tree
     return final_tree
 end
 
-function insertion(tree::Tree, m::Vector{Exp}, hashstate::HashState)
-    return insertion_aux(tree, m, length(m)+1, Dict{Tuple{Tree,Int}, Tree}(), hashstate)
+function insertion(tree::Tree, m::Monomial, hashstate::HashState)
+    return insertion_aux(tree, m, length(m.exps)+1, Dict{Tuple{Tree,Int}, Tree}(), hashstate)
 end
 
 
 # Function to create the staircase tree of a list of monomials
-function creation(monomial_list::Vector{Vector{Exp}})
+function creation(monomial_list::Vector{<:Monomial})
     tree = nothing_tree
     hashstate = new_hashstate()
     for m in monomial_list
@@ -147,6 +164,42 @@ function creation(monomial_list::Vector{Vector{Exp}})
     return tree
 end
 
+function list_reading(filename::String)
+    monomial_list = Monomial[]
+    open(filename, "r") do fichier
+        for line in eachline(fichier)
+            line = strip(line)
+            if !isempty(line)
+                lst = JSON.parse(line)
+                n = length(lst)
+                mon = monomial(SVector{n}(lst))
+                push!(monomial_list, mon)
+            end
+        end
+    end
+    return monomial_list
+end
 
-T = creation([Int16[1,1,1], Int16[0,2,2]])
-print_tree(T)
+function size_of_tree(tree::Tree)
+    if isempty(tree.edges) || tree == nothing_tree
+        return 0
+    end
+    return 1 + sum(size_of_tree(subtree[2]) for subtree in tree.edges)
+end
+
+function number_of_distinct_trees(tree::Tree, mem::Dict{Tree, Tree} = Dict{Tree,Tree}())
+    if isempty(tree.edges) || tree == nothing_tree
+        return 0
+    end
+    if haskey(mem, tree)
+        return 0
+    end
+    number = 0
+    for subtree in tree.edges
+        if !haskey(mem, subtree[2])
+            number += 1 + number_of_distinct_trees(subtree[2], mem)
+        end
+    end
+    mem[tree] = tree
+    return number
+end
