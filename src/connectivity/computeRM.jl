ee70bc5009b7670287ec91c01e9ff384176f8f0d
Remi Prebet <remi.prebet@lip6.fr>
2025-10-13 10:22:54 +0200

    move RM struct to types.jl + improve internal functions

--- src/connectivity/computeRM.jl
+++ src/connectivity/computeRM.jl
@@ -4,7 +4,7 @@
 #using AlgebraicSolving
 #using Nemo
 
-export roadmap, computepolar, MidRationalPoints, fbr, all_eqs, all_base_pts
+export roadmap, computepolar, MidRationalPoints, fbr, all_eqs, all_base_pts, nb_nodes
 include("Cannytools.jl")
 
 function roadmap(
@@ -110,54 +110,6 @@ end
 function fbr(I::Ideal{P} where P <: QQMPolyRingElem, Q::Vector{QQFieldElem})
     return fbr(I.gens, Q)
 end
-
-mutable struct RMnode
-    polar_eqs::Vector{QQMPolyRingElem}
-    base_pt::Vector{QQFieldElem}
-    children::Vector{RMnode}
-end
-
-mutable struct Roadmap
-    initial_ideal::Ideal{QQMPolyRingElem}
-    root::RMnode
-end
-
-function all_eqs(RM::Roadmap)
-    function all_eqs_rec(RMn::RMnode)
-        eqs = [fbr(vcat(RM.initial_ideal.gens, RMn.polar_eqs), RMn.base_pt)]
-        for child in RMn.children
-            append!(eqs, all_eqs_rec(child))
-        end
-        return eqs
-    end
-    return all_eqs_rec(RM.root)
-end
-
-function all_base_pts(RM::Roadmap)
-    function all_base_pts_rec(RMn::RMnode)
-        eqs = [RMn.base_pt]
-        for child in RMn.children
-            append!(eqs, all_base_pts_rec(child))
-        end
-        return eqs
-    end
-    return all_base_pts_rec(RM.root)
-end
-
-function nb_nodes(RM::Roadmap)
-    function nb_nodes_rec(RMn::RMnode)
-        nb = 1
-        for child in RMn.children
-            nb += nb_nodes_rec(child)
-        end
-        return nb
-    end
-    return nb_nodes_rec(RM.root)
-end
-
-Base.show(io::IO, RM::Roadmap) = print(io, all_base_pts(RM))
-Base.getindex(RM::Roadmap, idx::Union{Int, UnitRange}) = all_eqs(RM)[idx]
-Base.lastindex(RM::Roadmap) = nb_nodes(RM)
 
 #=
 ## Test ##
--- src/types.jl
+++ src/types.jl
@@ -77,3 +77,40 @@ Base.show(io::IO, I::Ideal) = print(io, I.gens)
 Base.getindex(I::Ideal, idx::Union{Int, UnitRange}) = I.gens[idx]
 
 Base.lastindex(I::Ideal) = lastindex(I.gens)
+
+mutable struct RMnode
+    polar_eqs::Vector{QQMPolyRingElem}
+    base_pt::Vector{QQFieldElem}
+    children::Vector{RMnode}
+end
+
+mutable struct Roadmap
+    initial_ideal::Ideal{QQMPolyRingElem}
+    root::RMnode
+end
+
+function _collect_roadmap(RMn::RMnode, F)
+    data = [F(RMn)]
+    for child in RMn.children
+        append!(data, _collect_roadmap(child, F))
+    end
+    return data
+end
+
+function all_eqs(RM::Roadmap)
+    func(s) = fbr(vcat(RM.initial_ideal.gens, s.polar_eqs), s.base_pt)
+    return _collect_roadmap(RM.root, func)
+end
+
+function all_base_pts(RM::Roadmap)
+    return _collect_roadmap(RM.root, s->s.base_pt)
+end
+
+function nb_nodes(RM::Roadmap)
+    return length(_collect_roadmap(RM.root, s -> true))
+end
+
+Base.show(io::IO, RM::Roadmap) = print(io, all_base_pts(RM))
+Base.getindex(RM::Roadmap, idx::Union{Int, UnitRange}) = all_eqs(RM)[idx]
+Base.lastindex(RM::Roadmap) = nb_nodes(RM)
+Base.length(RM::Roadmap) = nb_nodes(RM)
