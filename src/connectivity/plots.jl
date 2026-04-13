a6ddb2d25824bf9c3495b13bda81dfd089fb364e
Rémi P. <remi.prebet@ens-lyon.fr>
2026-04-13 21:32:38 +0200

    start plot export

--- src/connectivity/plots.jl
+++ src/connectivity/plots.jl
@@ -80,3 +80,100 @@ function replace_width_height_in_file(filename, x, y)
     # Write the modified content back to the file
     write(filename, content)
 end
+
+# ---------- TYPES ----------
+
+struct EdgeGroup
+    edges::Vector{Tuple{Vector{Float64}, Vector{Float64}}}
+    color::Int64#NTuple{3,Float64}
+    width::Float64
+end
+
+struct PointGroup
+    vertices::Tuple{Vector{Float64}, Vector{Float64}}  # (x, y)
+    color::Int64#NTuple{3,Float64}
+    marker::Symbol
+end
+
+struct GraphPlotData
+    edge_groups::Vector{EdgeGroup}
+    point_groups::Vector{PointGroup}
+end
+
+# ---------- HELPERS ----------
+
+function normalize_vemph(Vemph)
+    if Vemph isa Vector{Vector}
+        return Vemph
+    elseif Vemph isa Vector
+        return [Vemph]
+    elseif Vemph isa Dict
+        return collect(values(Vemph))
+    else
+        return Vector{Vector{Int}}()
+    end
+end
+
+# ---------- CORE BUILDERS ----------
+# TODO: remove the sequence in edge_groups as there should be only one, right? (same for point_groups in build_graph_data)
+
+function build_graph_data(G, Vemph=Vector{Vector{Int}}(); width=3.0, vert=true)
+    Vemph = normalize_vemph(Vemph)
+    V, E = G
+
+    # edges
+    edges = [
+        begin
+            v1, v2 = [map(Float64, V[idx]) for idx in e]
+            ([v1[1], v2[1]], [v1[2], v2[2]])
+        end
+        for e in E
+    ]
+
+    edge_groups = [EdgeGroup(edges, rand(1:666), width)]
+
+    #point groups
+    point_groups = PointGroup[]
+
+    if vert
+        vx = map(v -> Float64(v[1]), V)
+        vy = map(v -> Float64(v[2]), V)
+        push!(point_groups, PointGroup((vx, vy), rand(1:666), :diamond))
+    end
+
+    for group in Vemph
+        hx = map(j -> Float64(V[j][1]), group)
+        hy = map(j -> Float64(V[j][2]), group)
+        push!(point_groups, PointGroup((hx, hy), rand(1:666), :diamond))
+    end
+
+    return GraphPlotData(edge_groups, point_groups)
+end
+
+# ---------- MULTI GRAPHS ----------
+
+function build_graphs_data(CG; width=3.0, vert=true)
+    results = GraphPlotData[]
+
+    for item in CG
+
+        G, Vemph = length(item) == 2 ? item : item, Vector{Int}()
+        data = build_graph_data(G, Vemph; width=width, vert=vert)
+
+        # override edge color per graph
+        for eg in data.edge_groups
+            eg = EdgeGroup(eg.edges, rand(1:666), eg.width)
+        end
+
+        push!(results, data)
+    end
+
+    return results
+end
+
+# ---------- CONNECTED COMPONENTS ----------
+
+function build_graph_components_data(G, Vemph=Vector{Int}(); width=3.0, vert=true)
+    CG = connected_components(G, Vemph)
+    return build_graphs_data(CG; width=width, vert=vert)
+end
